5bb7da45e2bed95f667a5f6fd136a38dfea77a8b
garobcsi <gasparrobert0908@gmail.com>
2023-03-24 13:55:44 +0100

    Made deafult structure for Register and Login

--- src/laravel-vue/resources/js/pages/IndexPage.vue
+++ src/laravel-vue/resources/js/pages/IndexPage.vue
@@ -1,6 +1,9 @@
 <template>
-
+    <Login></Login>
+    <Register class="mt-3"></Register>
 </template>
 
 <script setup>
+import Login from "../components/auth/Login.vue";
+import Register from "../components/auth/Register.vue";
 </script>
