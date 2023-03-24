e5820fb0bd7c99653af30b0cc0d9c0d0b432cc6e
garobcsi <gasparrobert0908@gmail.com>
2023-03-24 11:52:14 +0100

    Controller Docs made

--- src/laravel-vue/app/Http/Controllers/UserController.php
+++ src/laravel-vue/app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Models\PersonalAccessToken;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Auth\Events\Login;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -15,12 +16,21 @@ use function PHPUnit\Framework\containsOnly;
 
 class UserController extends Controller
 {
+    /**
+     * @param RegisterRequest $request
+     * @return JsonResponse
+     */
     public function Register(RegisterRequest $request) {
         $data = $request->validated();
         $data["password"] = Hash::make($data["password"]);
         User::create($data);
         return response()->json(["message" => "Registration Success."],200);
     }
+
+    /**
+     * @param LoginRequest $request
+     * @return JsonResponse
+     */
     public function Login(LoginRequest $request) {
         if(Auth::attempt($request->validated())) {
             $user = Auth::user();
@@ -34,9 +44,19 @@ class UserController extends Controller
             return response()->json(["message" => "Login Unsuccessful."],401);
         }
     }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function IsLoginValid(Request $request) {
         return response()->json(["message" => "Authenticated."],200);
     }
+
+    /**
+     * @param $token
+     * @return void
+     */
     private function SetTokenExpiresAt($token) {
         $explode = explode('|',$token);
         $thisToken = count($explode) == 2 ? $explode[1] : $token;
@@ -45,14 +65,29 @@ class UserController extends Controller
         $data->expires_at = Carbon::now()->addDay();
         $data->save();
     }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function KeepTokenAlive(Request $request) {
         $this->SetTokenExpiresAt($request->bearerToken());
         return response()->json(["message" => "Token Kept Alive"],200);
     }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function Logout(Request $request) {
         $request->user()->currentAccessToken()->delete();
         return response()->json(["message" => "Logout Success."],200);
     }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function LogoutAll(Request $request) {
         $request->user()->tokens()->delete();
         return response()->json(["message" => "Logout All Success."],200);
