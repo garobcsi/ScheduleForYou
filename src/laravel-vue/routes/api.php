d8829576bc066f357d35c817be0a30823a417b6e
garobcsi <gasparrobert0908@gmail.com>
2023-03-24 13:55:24 +0100

    Made User api,

--- src/laravel-vue/routes/api.php
+++ src/laravel-vue/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
 });
 
 Route::post('/register',[UserController::class,'Register'])->name('user.Register');
