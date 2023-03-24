1756f9260b960941d09b986462eceee7172689b3
BalazsBenceLevente <112852999+BalazsBenceLevente@users.noreply.github.com>
2023-03-24 14:41:59 +0100

    NavBar (1.0)

---
+++ src/laravel-vue/resources/js/components/layout/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between
+    items-center">
+    
+      <div class="flex items-center">
+        <span class="text-green-500 text-xl mr-1">
+            <i class=""></i>
+        </span>
+        <h1 class="text-xl">Designer</h1>
+      </div>
+
+      <ul class="md:flex md:items-center ">
+        <li class="md:mx-4" v-for="link in Links">
+            <a :href="link.link" class="text-xl hover:text-green-500">{{link.name}}</a>
+        </li>
+      </ul>
+
+    </div> 
+
+</template>
+
+<script>
+export default{
+    setup(){
+        Links = [
+            {name : "Kezdőlap" , link : "#"},
+            {name : "Szolgáltatások" , link : "#"},
+            {name : "Rólunk" , link : "#"},
+            {name : "Blog" , link : "#"},
+            {name : "Elérhetőség" , link : "#"},
+        ]
+
+        return{Links}
+    }
+
+}
+
+</script>
+
