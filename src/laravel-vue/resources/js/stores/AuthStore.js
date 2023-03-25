9ed500d5c58c80cdc43bf3bf25b353a0ee983f49
garobcsi <gasparrobert0908@gmail.com>
2023-03-25 11:41:13 +0100

    Pinia: AuthStore updated

--- src/laravel-vue/resources/js/stores/AuthStore.js
+++ src/laravel-vue/resources/js/stores/AuthStore.js
@@ -1,15 +1,18 @@
 import { defineStore } from 'pinia'
 
-export const useCounter = defineStore('auth-store', {
+export const useAuthStore = defineStore('auth-store', {
     state() {
         return {
-
+            token: localStorage.getItem('token') !== null ? JSON.parse(localStorage.getItem('token')).data.token : null,
+            user: localStorage.getItem('user') !== null ? JSON.parse(localStorage.getItem('user')).data : null,
         }
     },
     actions: {
 
     },
     getters: {
-
+        isLogedIn() {
+            return this.token !== null;
+        }
     }
 })
