e52efea7478b7c3c8a2ac4e034898b9467e82f01
garobcsi <gasparrobert0908@gmail.com>
2023-03-25 15:32:07 +0100

    Made Register component

--- src/laravel-vue/resources/js/components/auth/Register.vue
+++ src/laravel-vue/resources/js/components/auth/Register.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { Form as VeeForm, Field, ErrorMessage } from 'vee-validate';
 import * as yup from 'yup';
+import {api} from "../../utils/api";
+import {ref} from "vue";
+import {router} from "../../router";
+import {value} from "lodash/seq";
+
+const alert_success = ref(null);
+const alert_danger = ref(null);
 const schema = yup.object({
     name:
         yup.string()
@@ -22,17 +29,57 @@ const schema = yup.object({
             .required('Kötelező kitölteni !')
             .oneOf([yup.ref('password')], 'A jelszavak nem egyeznek meg !')
 })
-function onSubmit(values) {
+async function onSubmit(values) {
     delete values.password_confirm;
-    console.log(values);
+    let data = null;
+    let error = null;
+    await api.post('/register',values).then(x=>data =x.data).catch(x=>error= x.response.data);
+    showAlert(data,error);
+}
+function onChange() {
+    alert_success.value = null;
+    alert_danger.value = null;
+}
+function showAlert(data,error) {
+    if (data !== null) {
+        if (data.message === "Registration Success.") {
+            alertSuccessRegisterOnIndex();
+        }
+    }
+    if (error !== null) {
+        if (error.message === "The email has already been taken.") {
+            alertDangerTaken();
+        }
+        else {
+            alertDangerUnexpected();
+        }
+    }
+}
+function alertSuccessRegisterOnIndex () {
+    alert_danger.value = null;
+    alert_success.value = 'Sikeres regisztráció !';
+}
+function alertDangerTaken() {
+    alert_success.value = null;
+    alert_danger.value = "Ez az email már foglalva van !";
+}
+function alertDangerUnexpected() {
+    alert_success.value = null;
+    alert_danger.value = "Váratlan hiba történt";
 }
 </script>
 
 <template>
-    <VeeForm @submit="onSubmit" v-slot="{ values }" :validation-schema="schema">
+    <VeeForm @submit="onSubmit" v-slot="{ values }" :validation-schema="schema" @change="onChange">
+        <div v-if="alert_success !== null" class="alert alert-success mb-0" role="alert">
+            {{alert_success}}
+        </div>
+        <div v-if="alert_danger !== null" class="alert alert-danger mb-0" role="alert">
+            {{alert_danger}}
+        </div>
         <label for="name" class="mt-2">Felhasználónév</label>
         <Field name="name" id="name" type="text" class="form-control mt-1" />
-        <ErrorMessage name="user_name" as="div" class="alert alert-danger mt-2 mb-0" />
+        <ErrorMessage name="name" as="div" class="alert alert-danger mt-2 mb-0" />
         <label for="email" class="mt-2">Email</label>
         <Field name="email" id="email" type="text" class="form-control mt-1" />
         <ErrorMessage name="email" as="div" class="alert alert-danger mt-2 mb-0" />
