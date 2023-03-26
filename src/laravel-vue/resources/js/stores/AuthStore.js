f2009cb1be5b848c06e2a192bf4e65f8d6954f3b
garobcsi <gasparrobert0908@gmail.com>
2023-03-26 12:08:07 +0200

    Pinia: Authentication made

--- src/laravel-vue/resources/js/stores/AuthStore.js
+++ src/laravel-vue/resources/js/stores/AuthStore.js
@@ -1,18 +1,14 @@
 import { defineStore } from 'pinia'
-
-export const useAuthStore = defineStore('auth-store', {
-    state() {
-        return {
-            token: localStorage.getItem('token') !== null ? JSON.parse(localStorage.getItem('token')).data.token : null,
-            user: localStorage.getItem('user') !== null ? JSON.parse(localStorage.getItem('user')).data : null,
-        }
-    },
-    actions: {
-
-    },
-    getters: {
-        isLogedIn() {
-            return this.token !== null;
-        }
-    }
+import {computed} from "vue";
+export const useAuthStore = defineStore('auth-store', () => {
+    const token = computed(() => {
+        return localStorage.getItem('token') !== null ? JSON.parse(localStorage.getItem('token')).data.token : null;
+    })
+    const user = computed(()=> {
+        return token.value !== null && localStorage.getItem('user') !== null ? JSON.parse(localStorage.getItem('user')).data : null;
+    })
+    const isLogedIn = computed(()=> {
+        return token.value !==null;
+    })
+    return {token,user,isLogedIn};
 })
