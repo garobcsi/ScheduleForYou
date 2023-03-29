a9bc77373198b76974e89976717119ac97855355
garobcsi <gasparrobert0908@gmail.com>
2023-03-29 13:00:14 +0200

    Made a logout button

--- src/laravel-vue/resources/js/pages/IndexPage.vue
+++ src/laravel-vue/resources/js/pages/IndexPage.vue
@@ -3,9 +3,15 @@
         <div class="row">
             <RouterLink :to="{name: 'login'}" class="btn btn-primary">To Login</RouterLink>
             <RouterLink :to="{name: 'register'}" class="btn btn-primary mt-1">To Registration</RouterLink>
+            <button class="btn btn-danger mt-1" @click="authStore.logout()">Logout</button>
+            {{authStore.token ?? "no data"}}
+            <br>
+            {{authStore.user ?? "no data"}}
         </div>
     </div>
 </template>
 
 <script setup>
+import {useAuthStore} from "../stores/AuthStore";
+const authStore = useAuthStore();
 </script>
