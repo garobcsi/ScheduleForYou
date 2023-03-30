79e55ded623afda95ad548f2285536c6fe8234fc
garobcsi <gasparrobert0908@gmail.com>
2023-03-30 18:12:03 +0200

    made on statrup validate token and keep it alive

--- src/laravel-vue/resources/js/logic/onStarup/auth.js
+++ src/laravel-vue/resources/js/logic/onStarup/auth.js
@@ -1,6 +1,7 @@
 import {useAuthStore} from "../../stores/AuthStore";
+import {api} from "../../utils/api";
 
-export function start() {
+export async function start() {
     const authStore = useAuthStore();
     const stayLogedIn = JSON.parse(localStorage.getItem('stayLogedIn'));
     if (stayLogedIn === null || !stayLogedIn) {
@@ -9,6 +10,22 @@ export function start() {
     }
     authStore.loadDataIn();
     if (!authStore.isLogedIn) {
+        authStore.logoutSession();
+        return;
+    }
+    let isValidData = null;
+    let isValidError= null;
+    await api.get('/login/valid').then(x=>isValidData =x.data).catch(x=>isValidError= x.response.data);
+    if (isValidData !== null && isValidData.message === "Authenticated.") {
+        let keepAliveData = null;
+        let keepAliveError = null;
+        await api.get('/login/alive').then(x=>keepAliveData =x.data).catch(x=>keepAliveError= x.response.data);
+        if (keepAliveError !== null) {
+            //error
+        }
+    }
+    else if(isValidError !== null){
+        //error
         authStore.logoutSession();
     }
 }
