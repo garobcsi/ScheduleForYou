1f91d9ebf7100414ca03b5716ada32febbf2a0bd
garobcsi <gasparrobert0908@gmail.com>
2023-03-31 12:21:28 +0200

    AlertPopup made

---
+++ src/laravel-vue/resources/js/components/alert/AlertBox.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="
+    col-12
+    offset-sm-6 col-sm-6
+    offset-md-8 col-md-4
+    offset-xl-9 col-xl-3 pb-3 pe-3 ps-sm-3"
+         id="AlertBox">
+        <AlertPopup :color="item.color" v-for="(item, index) in alertStore.data" :key="item.id" :pkey="item.id">
+            {{item.message}}
+        </AlertPopup>
+    </div>
+</template>
+
+<script setup>
+import {watch} from "vue";
+import AlertPopup from "../bootstrap/AlertPopup.vue";
+import {useAlertStore} from "../../stores/AlertStore";
+const alertStore = useAlertStore();
+watch(alertStore.data,() =>{
+    const alert = document.getElementById('AlertBox');
+    setTimeout(function() {alert.scrollTo({
+        top: alert.scrollHeight,
+        left:0,
+        behavior: "smooth"
+    });}, 1);
+
+});
+</script>
+
+<style scoped>
+#AlertBox {
+    position: fixed;
+    bottom: 0;
+    max-height: 100vh;
+    overflow: auto;
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
+#AlertBox::-webkit-scrollbar {
+    display: none;
+}
+</style>
--- src/laravel-vue/resources/js/pages/IndexPage.vue
+++ src/laravel-vue/resources/js/pages/IndexPage.vue
@@ -9,10 +9,26 @@
             <br>
             {{authStore.user ?? "no data"}}
         </div>
+            <button class="btn btn-primary" @click="addAlertT">Add Alert Type 1</button>
+            <button class="btn btn-primary" @click="addAlertTT">Add Alert Type 2</button>
+            <button class="btn btn-primary" @click="addAlertTTT">Add Alert Type 3</button>
     </div>
 </template>
 
 <script setup>
 import {useAuthStore} from "../stores/AuthStore";
+import {useAlertStore} from "../stores/AlertStore";
 const authStore = useAuthStore();
+const alertStore = useAlertStore();
+function addAlertT() {
+    alertStore.push("This is a msg !","warning");
+    console.log(alertStore.data);
+}
+function addAlertTT() {
+    alertStore.push("TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT ","danger");
+    console.log(alertStore.data);
+}function addAlertTTT() {
+    alertStore.push("Alert !","primary");
+    console.log(alertStore.data);
+}
 </script>
