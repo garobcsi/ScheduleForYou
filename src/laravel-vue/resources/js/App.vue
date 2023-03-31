14d90dfd1752bbadf1704ad1d4a2f39d35f27352
BalazsBenceLevente <112852999+BalazsBenceLevente@users.noreply.github.com>
2023-03-31 12:19:33 +0200

    Slider deleted

--- src/laravel-vue/resources/js/App.vue
+++ src/laravel-vue/resources/js/App.vue
@@ -1,11 +1,9 @@
 <template>
   <NavBar/>
-  <Slider/>
   <RouterView/>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
 import NavBar from './components/bootstrap/NavBar.vue';
-import Slider from './components/bootstrap/Slider.vue';
 </script>
