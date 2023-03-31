d1f5bf185a3001733d68f593c91662e773a5f09b
PSzDonat <p.sz.donat30@gmail.com>
2023-03-31 14:56:08 +0200

    basic-margo use

--- src/laravel-vue/resources/js/pages/IndexPage.vue
+++ src/laravel-vue/resources/js/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container basic-margo">
         <div class="row">
             <RouterLink :to="{name: 'login'}" class="btn btn-primary">To Login</RouterLink>
             <RouterLink :to="{name: 'register'}" class="btn btn-primary mt-1">To Registration</RouterLink>
