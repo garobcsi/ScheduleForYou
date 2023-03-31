49ac5e1a533198ffd0baea3b9c43d5420ded39aa
BalazsBenceLevente <112852999+BalazsBenceLevente@users.noreply.github.com>
2023-03-31 11:40:57 +0200

    Homepage created

--- src/laravel-vue/resources/js/App.vue
+++ src/laravel-vue/resources/js/App.vue
@@ -1,9 +1,11 @@
 <template>
   <NavBar/>
+  <Slider/>
   <RouterView/>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
 import NavBar from './components/bootstrap/NavBar.vue';
+import Slider from './components/bootstrap/Slider.vue';
 </script>
