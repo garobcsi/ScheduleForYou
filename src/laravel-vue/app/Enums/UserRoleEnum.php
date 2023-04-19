bb8de53e79e18eb10048c330493f76078e01a78a
garobcsi <gasparrobert0908@gmail.com>
2023-04-19 11:54:34 +0200

    Made new user enum. Changed api resposnse mesages. (bug fix)

---
+++ src/laravel-vue/app/Enums/UserRoleEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum UserRoleEnum:string
+{
+    case User = "user";
+    case Admin = "admin";
+
+}
--- src/laravel-vue/app/Http/Controllers/Admin/UserAdminController.php
+++ src/laravel-vue/app/Http/Controllers/Admin/UserAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\UserRoleEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
@@ -13,7 +14,7 @@ class UserAdminController extends Controller
         if(!in_array($request->host(),["localhost","127.0.0.1"])) return response()->json(["message" => "Cannot create account on ".$request->host()],403);
         $data = $request->validated();
         $data["password"] = Hash::make($data["password"]);
-        $data["role"] = "admin";
+        $data["role"] = UserRoleEnum::Admin;
         User::create($data);
         return response()->json(["message" => "Registration Success."],200);
     }
