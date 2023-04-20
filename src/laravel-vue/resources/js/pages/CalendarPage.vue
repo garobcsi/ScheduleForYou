a9975d8fe63630cc09d6d7e9d31119b60125ce70
BalazsBenceLevente <112852999+BalazsBenceLevente@users.noreply.github.com>
2023-04-20 15:08:41 +0200

    basic FullCalendar done

--- src/laravel-vue/resources/js/pages/CalendarPage.vue
+++ src/laravel-vue/resources/js/pages/CalendarPage.vue
@@ -49,7 +49,7 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-
+import { INITIAL_EVENTS, createEventId } from './event-utils'
 
 export default defineComponent({
     components: {
