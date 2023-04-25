602e0bee11522bb46f99b468957b11909f35998e
garobcsi <gasparrobert0908@gmail.com>
2023-04-25 19:01:36 +0200

    Made admin pages for admin users

---
+++ src/laravel-vue/resources/js/pages/Admin/AdminIndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container mt-2">
+        <div class="row">
+            <router-link class="btn btn-primary" :to="{name:'admin_login_registration_test'}">Login Registration Test</router-link>
+            <router-link class="btn btn-primary mt-2" :to="{name:'admin_toast_test'}">Toast Test</router-link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/laravel-vue/resources/js/router/index.js
+++ src/laravel-vue/resources/js/router/index.js
@@ -10,17 +10,13 @@ const routes = [
         path: '/login',
         name: 'login',
         component: () => import('@/pages/LoginPage.vue'),
-        meta: {
-            accessAfterLogin: false
-        }
+        beforeEnter: [dontAccessAfterLogin]
     },
     {
         path: '/register',
         name: 'register',
         component: () => import('@/pages/RegisterPage.vue'),
-        meta: {
-            accessAfterLogin: false
-        }
+        beforeEnter: [dontAccessAfterLogin]
     },
     {
         path: '/homepage',
@@ -28,37 +24,68 @@ const routes = [
         component: () => import('@/pages/HomePage.vue')
     },
     {
-        path: '/test',
-        name: 'test',
-        component: () => import('@/pages/TestPage.vue'),
-    },
-    {
         path: '/naptar',
         name: 'naptar',
         component: () => import('@/pages/CalendarPage.vue'),
     },
     {
-        path: '/frontend_test',
-        name: 'frontend_test',
-        component: () => import('@/pages/FrontendTestPage.vue')
-    },{
         path: '/profile',
         name: 'profile',
         component: () => import('@/pages/ProfilePage.vue')
     },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/components/error/NotFound.vue') }, // 404 error not found
+    // admin
+    {
+        path: '/admin',
+        name: 'admin_index',
+        component: () => import('@/pages/Admin/AdminIndexPage.vue'),
+        beforeEnter: [onlyAdminAccess]
+    },
+    {
+        path: '/admin/login_registration_test',
+        name: 'admin_login_registration_test',
+        component: () => import('@/pages/Admin/LoginRegistrationTestPage.vue'),
+        beforeEnter: [onlyAdminAccess]
+    },
+    {
+        path: '/admin/toast_test',
+        name: 'admin_toast_test',
+        component: () => import('@/pages/Admin/ToastTestPage.vue'),
+        beforeEnter: [onlyAdminAccess]
+    }
+    // admin end
 ]
 
 export const router = createRouter({
   history: createWebHistory(),
   routes
 });
-router.beforeEach((to, from, next)=>{
+function accessAfterLogin(to, from, next) {
     const authStore= useAuthStore();
-    if ((to.meta.accessAfterLogin === false && authStore.isLogedIn) || (to.meta.accessAfterLogin === true && !authStore.isLogedIn)) {
+    if (authStore.isLogedIn === true) {
+        next();
+    }
+    else {
+        next({name: 'index'});
+    }
+}
+function dontAccessAfterLogin(to, from, next) {
+    const authStore= useAuthStore();
+    if (authStore.isLogedIn === true) {
         next({name: 'index'});
     }
     else {
         next();
     }
-})
+}
+async function onlyAdminAccess(to, from, next) {
+    const authStore= useAuthStore();
+    let isAdmin = null;
+    await authStore.isAdmin.then(x=>isAdmin = x);
+    if (isAdmin === true) {
+        next();
+    }
+    else {
+        next({name: 'index'});
+    }
+}
