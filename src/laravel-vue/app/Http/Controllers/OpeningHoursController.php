1767d93c3947f8bda667e46ade753626b2a62340
PSzDonat <p.sz.donat30@gmail.com>
2023-05-08 17:50:08 +0200

    OpeningHoursController Index

---
+++ src/laravel-vue/app/Http/Controllers/OpeningHoursController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\OpeningHoursRequest;
+use App\Models\Company;
+use App\Models\CompanyOpeningHours;
+use Illuminate\Http\Request;
+
+class OpeningHoursController extends Controller
+{
+    public function index(){
+        return response()->json(["data" => CompanyOpeningHours::all()],200);
+    }
+}
