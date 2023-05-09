3913c21c7a050732d3f20a23b85199ecbd2d4c10
garobcsi <gasparrobert0908@gmail.com>
2023-05-09 12:52:02 +0200

    Moved To Trait Folder

--- src/laravel-vue/app/Enums/RepeatTimeHUEnum.php
+++ src/laravel-vue/app/Enums/RepeatTimeHUEnum.php
@@ -2,36 +2,11 @@
 
 namespace App\Enums;
 
-trait EnumToArrayHU
-{
-
-    public static function names(): array
-    {
-        return array_column(self::cases(), 'name');
-    }
-
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'value');
-    }
-
-    public static function array(): array
-    {
-        $arr = array();
-        foreach (self::names() as $n => $i) {
-            $arr[] = [
-                "name" => $i,
-                "value" => self::values()[$n]
-            ];
-        }
-        return $arr;
-    }
-
-}
+use App\Trait\EnumToArray;
 
 enum RepeatTimeHUEnum:string
 {
-    use EnumToArrayHU;
+    use EnumToArray;
     case Naponta = "daily";
     case Hetente = "weekly";
     case Hónaponta = "monthly";
