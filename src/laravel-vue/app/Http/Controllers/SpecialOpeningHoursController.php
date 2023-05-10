4e237e6765655da8ab14042aac5f90a95f987746
PSzDonat <p.sz.donat30@gmail.com>
2023-05-10 17:38:33 +0200

    SOHC fix

--- src/laravel-vue/app/Http/Controllers/SpecialOpeningHoursController.php
+++ src/laravel-vue/app/Http/Controllers/SpecialOpeningHoursController.php
@@ -5,14 +5,35 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SpecialOpeningHoursRequest;
 use App\Models\Company;
 use App\Models\CompanySpecialOpeningHours;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SpecialOpeningHoursController extends Controller
 {
-    public function index(){
-        return response()->json(["data" => CompanySpecialOpeningHours::all()],200);
+    public function index(Company $company): JsonResponse
+    {
+        return response()->json(["data" => CompanySpecialOpeningHours::whereRelation('CompanyOpeningHours','companies_id',$company->id)],200);
     }
-    public function show(CompanySpecialOpeningHours $specialOH){
-        return response()->json(["data" => $specialOH]);
+    public function show(CompanySpecialOpeningHours $id): JsonResponse
+    {
+        return response()->json(["data" => $id],200);
+    }
+    public function update(SpecialOpeningHoursRequest $request, Company $company, CompanySpecialOpeningHours $SpecialOH): JsonResponse
+    {
+        $this->authorize('onlyOwnerCoOwnerManager',$company);
+        $data = $request->validated();
+        $SpecialOH->start = $data["start"];
+        $SpecialOH->end = $data["end"];
+        $SpecialOH->open_or_close = $data["open_or_close"];
+        $data->save();
+        return response()->json(["message" => "Data updated successfully."],201);
+    }
+    public function destroy(Company $company, CompanySpecialOpeningHours $SpecialOHID): JsonResponse
+    {
+        $this->authorize('onlyOwnerCoOwner',$company);
+        dd($company->OpeningHours->CompanySpecialOpeningHours);
+        $SpecialOHID->delete();
+        return response()->json(["message" => "Deleted successfully."],200);
     }
 }
