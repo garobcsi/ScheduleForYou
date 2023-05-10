0404bf80e331e1a3e63e27773a005767efda0d90
PSzDonat <p.sz.donat30@gmail.com>
2023-05-10 13:37:28 +0200

    create SpecialOpeningHoursController

---
+++ src/laravel-vue/app/Http/Controllers/SpecialOpeningHoursController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SpecialOpeningHoursRequest;
+use App\Models\Company;
+use App\Models\CompanySpecialOpeningHours;
+use Illuminate\Http\Request;
+
+class SpecialOpeningHoursController extends Controller
+{
+}
