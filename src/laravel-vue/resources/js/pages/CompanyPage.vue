f8a1bd0cb400fca5dd1671155ed863f96cb14c40
PSzDonat <p.sz.donat30@gmail.com>
2023-05-12 16:01:02 +0200

    companyPage planning

---
+++ src/laravel-vue/resources/js/pages/CompanyPage.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+<!--            <div class="container py-5">-->
+<!--                <div class="row">-->
+<!--                    <div class="col-md-8">-->
+<!--                        <h1 class="mb-4">{{ company.name }}</h1>-->
+<!--                        <p>{{ company.introduce }}</p>-->
+<!--                        <p>Address: {{ company.address }}</p>-->
+<!--                        <p>Phone: {{ company.tel }}</p>-->
+<!--                        <p>Email: {{ company.email }}</p>-->
+<!--                        <h2 class="mt-5">Opening Hours</h2>-->
+<!--                        <table class="table">-->
+<!--                            <thead>-->
+<!--                            <tr>-->
+<!--                                <th>Day</th>-->
+<!--                                <th>Open</th>-->
+<!--                                <th>Close</th>-->
+<!--                            </tr>-->
+<!--                            </thead>-->
+<!--                            <tbody>-->
+
+<!--                            </tbody>-->
+<!--                        </table>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+        </div>
+    </div>
+</template>
+
+<script setup>
+// import {useI18n} from "vue-i18n";
+// import {api} from "../utils/api";
+// import {ref} from "vue";
+//
+// const { t, te } = useI18n();
+// const company = ref({});
+//
+// (async () => {
+//
+// })();
+</script>
+
+<script>
+// import {api} from "../utils/api";
+//
+// export default {
+//     data(){
+//         return{
+//             company:{}
+//         }
+//     },
+//     async mounted() {
+//
+//         await api.get(`/company/${this.$route.params.id}`).then(x=>this.company =x.data.data)
+//         console.log(this.$route.params.id)
+//     }
+// }
+</script>
+
+<style scoped>
+
+</style>
